822de098a473e3feeeb35fefcbaa68546a6cb2df
Enzo Hideo <enzohideo@gmail.com>
2023-06-03 18:58:17 -0300

    style(Explosion): remove bad smells
    Done automatically by the linter:
    
        removed trailing whitespace, fixed indentation, removed/added
        missing imports.
    
    Done manually:
    
        Added empty curly braces to Scoreboard.json and Leaderborard.json,
        so it's syntactically acepted as a JSON file by the linter.
    
        Changed the methods' names from snake_case to camelCase so it
        matches the standard required by the linter and the style used in
        the rest of the project.

--- app/src/main/kotlin/galaxyraiders/core/game/Explosion.kt
+++ app/src/main/kotlin/galaxyraiders/core/game/Explosion.kt
@@ -1,21 +1,24 @@
 package galaxyraiders.core.game
 
-import galaxyraiders.core.physics.Object2D
 import galaxyraiders.core.physics.Point2D
 import galaxyraiders.core.physics.Vector2D
 
-class Explosion(initialPosition: Point2D, initialVelocity : Vector2D, radius: Double, mass: Double) : 
+class Explosion(
+  initialPosition: Point2D,
+  initialVelocity: Vector2D,
+  radius: Double,
+  mass: Double
+) :
   SpaceObject("Explosion", '+', initialPosition, initialVelocity, radius, mass) {
-    
-    var trigger : Boolean = false
-    
-    fun set_trigerred(bool: Boolean) : Unit {
-      this.trigger = bool
-      return Unit
-    }
 
-    fun is_trigerred() : Boolean {
-      return this.trigger
-    }
+  var trigger: Boolean = false
 
+  fun setTriggered(bool: Boolean) {
+    this.trigger = bool
+    return Unit
+  }
+
+  fun isTriggered(): Boolean {
+    return this.trigger
+  }
 }
